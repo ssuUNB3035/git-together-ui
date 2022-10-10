8d8b925379c9037269c02bd43869b3b4fab91506
ZekyTeky <ssu@unb.ca>
2022-10-10 14:49:24 -0300

    initial code

---
+++ app/src/main/java/ca/unb/mobiledev/gittogetherui/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package ca.unb.mobiledev.gittogetherui.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
